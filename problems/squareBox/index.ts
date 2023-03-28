18135a1c69307580c61bbaef49c4847b7e53a521
tranlehaiquan <tranlehaiquan@gmail.com>
2023-03-29 00:23:24 +0700

    add problems

---
+++ problems/squareBox/index.ts
@@ -0,0 +1,20 @@
+// You want to divide a farm evenly into squares
+// the plots to be big as possible
+
+const solution = (farm: number[]): number => {
+  const length = Math.max(farm[0], farm[1]);
+  const width = Math.min(farm[0], farm[1]);
+  // base case
+  if (length % width === 0) {
+    // console.log(`Possible with ${width}`);
+    return width;
+  }
+
+  // recursion case
+  // make the farm smaller
+  const newFarm: [number, number] = [width, length - width];
+  // console.log(`Make farm smaller ${newFarm}`);
+  return solution(newFarm);
+};
+
+export default solution;
---
+++ problems/squareBox/squareBox.test.ts
@@ -0,0 +1,31 @@
+import solution from ".";
+
+describe("squareBox", () => {
+  // test case 1
+  it("should return 1", () => {
+    const farm = [1, 1];
+    const result = solution(farm);
+    expect(result).toEqual(1);
+  });
+
+  // test case 2
+  it("should return 2", () => {
+    const farm = [2, 2];
+    const result = solution(farm);
+    expect(result).toEqual(2);
+  });
+
+  // test case 3
+  it("should return 2", () => {
+    const farm = [2, 4];
+    const result = solution(farm);
+    expect(result).toEqual(2);
+  });
+
+  // test case 4
+  it("should return 80", () => {
+    const farm = [1680, 640];
+    const result = solution(farm);
+    expect(result).toEqual(80);
+  });
+});
