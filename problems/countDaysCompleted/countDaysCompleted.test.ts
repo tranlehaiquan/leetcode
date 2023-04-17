229fe8b68e44ad33ec55942d384b1bd4fee77a92
tranlehaiquan <tranlehaiquan@gmail.com>
2023-04-17 20:48:00 +0700

    Add problem an test

--- problems/countDaysCompleted/countDaysCompleted.test.ts
+++ problems/countDaysCompleted/countDaysCompleted.test.ts
@@ -1,5 +1,13 @@
-describe('test', () => {
-  it('test', () => {
-    expect(1).toBe(1);
+import solution from ".";
+
+describe("countDaysCompleted", () => {
+  it("should return 3", () => {
+    expect(solution([5, 8, 2, 7], 3)).toBe(3);
   });
-})
+  it("should return 3", () => {
+    expect(solution([2, 5, 9, 2, 1, 4], 4)).toBe(3);
+  });
+  it("should return 4", () => {
+    expect(solution([1, 12, 10, 4, 5, 2], 2)).toBe(4);
+  });
+});
--- problems/countDaysCompleted/index.ts
+++ problems/countDaysCompleted/index.ts
@@ -1,10 +1,14 @@
-// In order to finish a game, a player has to complete N missions. The missions are numbered from 0 to N-1. The K-th mission has an integer D[K] assigned, representing its difficulty level.
+// In order to finish a game, a player has to complete N missions.
+// The missions are numbered from 0 to N-1. The K-th mission has an integer D[K] assigned, representing its difficulty level.
 // During a day, you can perform any number of missions given the two following rules:
-// • missions should be performed in the specified order, in other words, a mission can be undertaken only if all of the missions preceding it have already been completed;
-// • the difference between the difficulty levels of any two missions performed on the same day should not be greater than an integer X.
+// • missions should be performed in the specified order, in other words,
+//   a mission can be undertaken only if all of the missions preceding it have already been completed;
+// • the difference between the difficulty levels of any two missions performed
+//   on the same day should not be greater than an integer X.
 // Write a function:
 // class Solution { public int solution(int(J D, int X); )
-// that, given an array D of N integers and an integer X, returns the minimum number of days required to complete all of the missions in the game.
+// that, given an array D of N integers and an integer X,
+// returns the minimum number of days required to complete all of the missions in the game.
 // Examples:
 // 1. Given D = [5, 8, 2, 7] and X = 3, your function should return 3. The first two missions can be
 // performed on the first day, the third mission on the second day and the last mission on the third day.
@@ -17,3 +21,26 @@
 // N is an integer within the range [1..200,000);
 // X is an integer within the range [0.. 1,000,000,000];
 // each element of array D is an integer within the range [1.. 1,000,000,000].
+
+const solution = (D: number[], X: number): number => {
+  let result = 1;
+  // previous using to store min, max
+  let previous: number[] = [];
+
+  for (const quest of D) {
+    const min = Math.min(...previous, quest);
+    const max = Math.max(...previous, quest);
+    const diff = Math.abs(max - min);
+
+    if (diff > X) {
+      result += 1;
+      previous = [quest];
+    } else {
+      previous = [min, max];
+    }
+  }
+
+  return result;
+};
+
+export default solution;
