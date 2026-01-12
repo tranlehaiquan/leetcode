2e28f599b6cfb7bc7a8d905295805300d3690d46
Jimmy Tran <tranlehaiquan@gmail.com>
2026-01-12 22:10:34 +0700

    feat: add trapTwoPointers function for optimized rainwater trapping calculation

--- leetcode/42-trapping-rain-water/index.ts
+++ leetcode/42-trapping-rain-water/index.ts
@@ -28,4 +28,30 @@ function trap(heights: number[]): number {
   return total;
 }
 
+export function trapTwoPointers(height: number[]): number {
+  if (!height || height.length === 0) {
+    return 0;
+  }
+
+  let l = 0;
+  let r = height.length - 1;
+  let maxL = height[l];
+  let maxR = height[r];
+  let total = 0;
+
+  while (l < r) {
+    if (height[l] < height[r]) {
+      l++;
+      maxL = Math.max(maxL, height[l]);
+      total += maxL - height[l];
+    } else {
+      r--;
+      maxR = Math.max(maxR, height[r]);
+      total += maxR - height[r];
+    }
+  }
+
+  return total;
+}
+
 export default trap;
